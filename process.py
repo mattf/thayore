3394edc988ee9b70de86d3e4a591246d640a7e64
Matthew Farrellee <matt@redhat.com>
2015-02-09 14:57:09 -0500

    add simple process, walks model

---
+++ process.py
@@ -0,0 +1,27 @@
+import time
+import random
+
+import model
+
+nodes = model.load('layout.dot')
+
+for n in nodes:
+    print n.name, map(lambda n: n.name, n.edges)
+
+def next(node):
+    return random.choice(node.edges.keys())
+
+def move(node):
+    return random.choice(filter(lambda n: n.name != node.name, node.edges.keys()))
+
+node = random.choice(nodes)
+node = reduce(lambda n, x: n.name == 'foyer' and n or x, nodes)
+now = 0
+sample_rate = .5 # in seconds
+acceleration = 10
+move_barrier = 30 * 60 # in seconds
+while True:
+    now += sample_rate
+    print now, node.name
+    node = now % move_barrier == 0 and move(node) or next(node)
+    time.sleep(sample_rate / acceleration)
