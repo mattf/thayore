86389e3b95e804c531cb2bd3392fc9301908dde6
Matthew Farrellee <matt@redhat.com>
2015-02-11 13:53:53 -0500

    arg parsing for receiver.py

--- receiver.py
+++ receiver.py
@@ -1,14 +1,21 @@
+import optparse
+
 from qpid.messaging import Connection, MessagingError
 
+parser = optparse.OptionParser(
+    "usage: %prog [broker url]",
+    description="Receive published events from simulators")
+parser.add_option("-a", "--address", default="amq.topic",
+                  help="Receive events on the given address")
+opts, args = parser.parse_args()
 
-broker = "localhost:5672"
-address = "amq.topic"
+broker = args and args.pop(0) or "localhost:5672"
+
 connection = Connection(broker)
-
 try:
     connection.open()
     session = connection.session()
-    receiver = session.receiver(address)
+    receiver = session.receiver(opts.address)
 
     while True:
         message = receiver.fetch()
